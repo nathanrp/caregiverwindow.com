ce79150cd2168c7eded209da867b86b0b1a0fd46
nathanrp <Nathan@Nathans-MacBook-Pro.local>
2014-08-08 14:56:31 -0600

    updates

--- index.php
+++ index.php
@@ -18,14 +18,16 @@ $title = 'Caregiver Window';
 <?php include ($path.'inc/header.php') ?>
 
 <div class="home-hero">
-	<img src="<?php echo $path;?>img/1.jpg" class="hero" alt="Banner" />
+	<div class="container">
+		<h1>Caregiver Window is the most user-friendly private-duty software available today.</h1>
+		<a class="btn">Try <span class="hidden-sm">Caregiver Window</span> Free for 30 Days</a>
+	</div>
 </div>
 
 <div class="content">
-	<section class="home-intro container">
-		<h1>Caregiver Window is the most user friendly private duty software available today.</h1>
-		<a class="btn">Try Caregiver Window Free for 30 Days</a>
-	</section>
+	<div class="container">
+		
+	</div>
 </div>
 
 <?php include ($path.'inc/footer.php') ?>
