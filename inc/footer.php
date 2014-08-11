e7800e18bd44b568e6e7d0764bdb2499a88885ef
nathanrp <Nathan@Nathans-MacBook-Pro.local>
2014-08-11 14:34:49 -0600

    design updates

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,8 @@
 <footer>
-	<ul>
-		<li><a>About Us</a></li>
-	</ul>
-	<p>&copy; 2014 Caregiver Window</p>
+	<div class="container">
+		<ul>
+			<li><a>About Us</a></li>
+		</ul>
+		<p>&copy; 2014 Caregiver Window</p>
+	</div>
 </footer>
--- index.php
+++ index.php
@@ -20,13 +20,54 @@ $title = 'Caregiver Window';
 <div class="home-hero">
 	<div class="container">
 		<h1>Caregiver Window is the most user-friendly private-duty software available today.</h1>
-		<a class="btn">Try <span class="hidden-sm">Caregiver Window</span> Free for 30 Days</a>
+		<div class="cta">
+			<a class="btn">Try <span class="hidden-sm">Caregiver Window</span> Free for 30 Days</a>
+		</div>
 	</div>
 </div>
 
 <div class="content">
 	<div class="container">
-		
+		<div class="home-features row">
+			<div class="col-xs-12 col-sm-6 feature">
+				<div class="photo"></div>
+				<h3>Easy &amp; Simple</h3>
+				<ul class="ul">
+					<li>No setup cost, and no per client charge</li>
+					<li>Web-based and accessible on all smart devices: iPad, iPhone, and Android devices</li>
+					<li>Online care assessments</li>
+					<li>Care provider scheduling with integrated time tracking</li>
+					<li>Communicate with staff via email and text</li>
+					<li><abbr>HR</abbr> solutions</li>
+					<li>Integrated billing and payroll</li>
+					<li>Access for families, caregivers, and referrers</li>
+				</ul>
+			</div>
+			<div class="col-xs-12 col-sm-6 feature">
+				<div class="photo"></div>
+				<h3>Back Office</h3>
+				<ul class="ul">
+					<li>Export billing and payroll to QuickBooks or your payroll company</li>
+					<li>Print invoices</li>
+					<li>See schedules and care documentation for clients that you referred</li>
+					<li>View reports to see profitability, client accounting, caregiver needs</li>
+					<li>Caregivers are filtered by client care needs and distance</li>
+					<li>Real-time updates alerting you and caregivers if they are late</li>
+					<li>HIPAA compliant</li>
+					<li>Real-time backups</li>
+				</ul>
+			</div>
+		</div>
+		<div class="cta">
+			<a class="btn">Try <span class="hidden-sm">Caregiver Window</span> Free for 30 Days</a>
+		</div>
+		<h2>Your Business</h2>
+<p>Caregiver Window positions you to become a leader in this market by helping you focus on those things that are critical to your success. Finally, make your back office part of your front office. Caregiver Window is a web-based system for client integration and management as well as caregiver scheduling, tracking and messaging, resulting in a symbiotic relationship that is transparent. It has been designed and fostered from 10 years of home care experience and will prove essential to your success.</p>
+<p>You need to know how to lead, manage, and operate your business smarter than your competition. Caregiver Window offers you this competitive edge. Caregiver window is an ideal solution for your private duty company. Instead of making mistakes between the two, you will have a critical component of your business in place that will allow you to develop to a deeply rewarding business.</p>
+<p>Your result with Caregiver Window will be more efficient operations and lower cost resulting in higher quality of care. In addition, it will allow for more time with your clients and referrals and an increased profit.</p>
+			<div class="cta">
+				<a class="btn">Try <span class="hidden-sm">Caregiver Window</span> Free for 30 Days</a>
+			</div>
 	</div>
 </div>
 
