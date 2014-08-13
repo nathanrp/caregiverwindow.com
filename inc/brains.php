8e98881c22e2663801902d3d5ae1af61449c9fc4
nathanrp <Nathan@Nathans-MacBook-Pro.local>
2014-08-13 12:16:43 -0600

    change fonts

--- inc/brains.php
+++ inc/brains.php
@@ -7,6 +7,6 @@
 <meta http-equiv="cleartype" content="on">
 
 <link rel="stylesheet/less" type="text/css" href="css/cgw.less">
-<link href='http://fonts.googleapis.com/css?family=Maven+Pro:400,700,500,900' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
 
 <script src="js/less.min.js" type="text/javascript"></script>
