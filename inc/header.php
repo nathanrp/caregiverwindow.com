751e27d90c546e9d77c204ae06b9209887963db1
Nathan <nathanphilpot@gmail.com>
2015-03-16 22:35:15 -0600

    updates

--- inc/header.php
+++ inc/header.php
@@ -1,10 +1,15 @@
 <header class="header <?php if($page=='0') echo 'home-header';?>">
-	<h1><a href="<?php echo $path;?>">Caregiver Window</a></h1>
+	<h1 class="<?php if($page=='0') echo 'home';?>">
+		<a href="<?php echo $path;?>">
+			<span>Caregiver</span>
+			<span>Window</span>
+		</a>
+	</h1>
 	<nav>
 		<ul>
 			<li class="<?php if($page=='1') echo 'active';?>"><a href="<?php echo $path;?>pricing/">Pricing</a></li>
 			<li><a href="<?php echo $path;?>support/">Support</a></li>
-			<li><a class="btn" href="<?php echo $path;?>app/">Sign In</a></li>
+			<li><a class="btn" href="<?php echo $path;?>https://app.caregiverwindow.com">Sign In</a></li>
 		</ul>
 	</nav>
 </header>
